e7b5980e51d86f7d7e037cfbdfd748f2fee05d36
dfdanielafigueroa0 <108244978+dfdanielafigueroa0@users.noreply.github.com>
2022-09-18 22:10:22 -0300

    Add files via upload

---
+++ switch-statement.js
@@ -0,0 +1,37 @@
+ var day='Sunday';
+
+switch (day) {
+    case 'Monday':
+        console.log('Work out,');
+        break;
+
+    case 'Tuesday':
+        console.log('Study');
+        break;
+    
+    case 'Wednesday':
+    console.log('Work out, study');
+    break;
+
+    case 'Thursday':
+    console.log('Study');
+    break;
+
+    case 'Friday':
+    console.log ('Chill at night');
+    break;
+
+    case 'Saturday':
+    console.log('Work out, have some rest');
+    break;
+
+    case 'Sunday':
+    console.log('Runing errands, Skin care');
+    break;
+    default:
+        console.log('There is no such a day');
+        
+
+}
+
+
