22e100db0999dbcae4c4b280bcea5d7b916d6c25
dfdanielafigueroa0 <108244978+dfdanielafigueroa0@users.noreply.github.com>
2022-09-18 22:11:43 -0300

    Add files via upload

---
+++ for-and-while-loops.js
@@ -0,0 +1,72 @@
+/* 
+Task 1
+Write a "for" loop and a "while" that will perform exactly the same repetitive code as this:
+
+console.log(1)
+console.log(2)
+console.log(3)
+console.log(4)
+console.log(5)
+console.log('Counting completed!');
+
+Answer: 
+
+for (i=1; i<=5; i++) {
+    console.log(i)
+}
+console.log('Counting completed')
+
+
+i=1;
+while (i <= 5){
+    console.log(i);
+    i= i++
+}
+console.log('Counting completed')
+
+
+Task 2 
+Write a "for" loop and a "while" loop that will perform exactly the same repetitive code as this:
+
+console.log(5)
+console.log(4)
+console.log(3)
+console.log(2)
+console.log(1)
+console.log('Countdown finished!');
+
+
+
+for (i=5; i>0; i--) {
+    console.log(i)
+}
+console.log('Countdown finished!')
+
+
+i= 5;
+while (i >=1){
+    console.log(i)
+    i = i-1
+}
+console.log('Countdown finished!')
+
+
+Task 3
+
+Write a "while" loop that will perform exactly the same repetitive code as this:
+console.log(2018)
+console.log(2019)
+console.log(2020)
+console.log(2021)
+console.log(2022)
+
+
+
+year= 2018;
+
+while (year <=2022){
+    console.log (year)
+    year= year+1
+}
+
+*/
